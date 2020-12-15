062b72543f7c6c3c00dd17492dae31f9beedbfac
katwithaC <catherinerosearredondo@gmail.com>
2020-12-15 00:52:10 -0600

    adding properties and metods to Employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,30 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constrctor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    //getter method
+    getName() {
+        return this.name
+    }
+
+    getID() {
+        return this.id
+    }
+
+    getEmail() {
+        return this.email
+    }
+
+    getRole() {
+        return "Employee"
+    }
+
+}
+
+
+module.exports = Employee;
